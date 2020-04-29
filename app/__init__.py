baf4e920390dd81992472e08e0226657de5e5f79
jadielmwangi <jedielmwangi@gmail.com>
2020-04-29 10:57:13 +0300

    SQLAlchemy to update entries from our database

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,35 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
-from .config import DevConfig
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
 
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
 
-from app import views
-from app import error
+
+
+    return app
